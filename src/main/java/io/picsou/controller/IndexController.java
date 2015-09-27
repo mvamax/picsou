bbe5da851e634a7b2ac5c9cb7ea7eb8b7d6b7519
alexis <alexis@alexis-desktop>
2015-09-27 21:02:52 +0200

    indexController

---
+++ src/main/java/io/picsou/controller/IndexController.java
@@ -0,0 +1,17 @@
+package io.picsou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping( value="/index")
+	public String index(){
+		
+		
+		return "index";
+		
+	}
+	
+}
